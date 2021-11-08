0e7c5b1acd63f211f2ec098be5198351ab37dc9e
mosi14 <m.asadollahy@hotmail.com>
2021-11-09 00:01:33 +0100

    S202107SPG-73: create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./Components/MyNav";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The dream team
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Route path="/" render={() => <> <MyNav  IsLogin={false} /> </>} />
+         </Router>
+    </>
   );
 }
 
---
+++ client/src/Components/MyNav.js
@@ -0,0 +1,35 @@
+import { Navbar, Container, Button } from "react-bootstrap";
+import dayjs from "dayjs";
+import { iconStar, iconPerson ,iconCalendar} from "./Icons";
+
+export default function MyNav(props) {
+  return (
+    <>
+      <Navbar
+        className="pr-2 justify-content-between NavBar-Background text-warning"
+        fixed="top"
+        expand="sm"
+      >
+        <Container fluid>
+          <Navbar.Brand>
+            {iconStar}
+            <span> SPG - Group 07</span>
+          </Navbar.Brand>
+          <small>Curent Date: {dayjs().format("DD/MM/YYYY").toString() }{"  "}{iconCalendar}</small>
+          <Navbar.Text>
+            {iconPerson}{" "}
+            {props.IsLogin ? (
+              <Button variant="link">Logout</Button>
+            ) : (
+              <>
+                {" "}
+                <Button variant="link">Login</Button>
+                <Button variant="link">Register</Button>{" "}
+              </>
+            )}{" "}
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
