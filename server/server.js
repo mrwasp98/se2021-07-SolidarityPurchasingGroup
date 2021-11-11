7d772906c310887482ab4d41c6b2ebd5d6c3939a
kecco98 <kecco98@outlook.com>
2021-11-11 17:50:04 +0100

    Backend login

--- server/server.js
+++ server/server.js
@@ -8,12 +8,65 @@ const orderDao = require('./order-dao');
 const productDao = require('./product-dao');
 const farmerDao = require('./farmer-dao');
 
+const passport = require('passport'); //auth middleware
+const LocalStrategy = require('passport-local').Strategy; //Username and password for login
+const session = require('express-session'); //enable sessions
+const userDao = require('./user-dao'); //module fo accessing the users in the DB
+
+/*** Set up Passport ***/
+
+//Inizializa and configure passport
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+      userDao.getUser(username,password).then((user) =>{
+          if(!user)
+            return done(null, false, {message: 'incorrect username and/or password.'});
+          return done(null, user);
+      });
+  }
+));
+
+//Serialize and deserialize user (user object <-> session)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+}),
+
+//starting from the data in the session, we extract the current (logged-id) user
+passport.deserializeUser((id, done) => {
+  userDao.getUserById(id).then( user => {
+    done(null, user); //This will be available in req.user
+  }).catch(err => {
+    done(err, null);
+  });
+});
+
 // init express
 const app = new express();
 const port = 3001;
 // set-up the middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+
+//Custom middleware: check if a given request is coming from an authenticated user
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated())
+      return next();
+
+    return res.status(401).json({error: 'not authenticated' });
+}
+
+//set up the session
+app.use(session({
+  //by defoult, Passport uses a MemoryStore to keep track of the sessions
+  secret: 'a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+//Then init passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*TEST GET route */
 app.get('/api/test', (req, res) => {
@@ -66,4 +119,27 @@ app.get('/api/farmers',
     farmerDao.getFarmers()
       .then(farmers => res.json(farmers))
       .catch(()=> res.staus(500).end());
+});
+
+/*** User APIs ***/
+
+//Login
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  //If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  res.json(req.user);
+});
+
+//DELETE /logout
+//logout
+app.delete('/logout', (req,res) => {
+req.logout();
+res.end();
+});
+
+app.get('/api/sessions/current', (req, res) => {
+if(req.isAuthenticated()) {
+  res.status(200).json(req.user);}
+else
+  res.status(401).json({error: 'Unauthenticated user!'});;
 });
