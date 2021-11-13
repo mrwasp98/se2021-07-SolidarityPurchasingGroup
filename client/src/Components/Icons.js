5d446183d7cebecffe5a13d7dbdfea325f7c28f2
giacomino100 <giacomo.inghilleri@gmail.com>
2021-11-13 21:39:29 +0100

    icons

--- client/src/Components/Icons.js
+++ client/src/Components/Icons.js
@@ -80,7 +80,7 @@ const iconAddDisabled = (<svg xmlns="http://www.w3.org/2000/svg" width="20" heig
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
 </svg>);
 
-const iconSub = (<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#143642" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+const iconSub = (<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#143642" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z" />
 </svg>)
 
