ba2ee360d3f2b50fe20547838e800ec982ffc18c
mosi14 <m.asadollahy@hotmail.com>
2021-11-16 01:48:37 +0100

    fix clients test

--- server/tests/api-clients.test.js
+++ server/tests/api-clients.test.js
@@ -5,6 +5,10 @@ const request = require("supertest");
 // we also need our app for the correct routes
 const app = require("../server.js");
 const clientDao = require('../client-dao');
+const userDao = require('../user-dao');
+let userId1;
+let userId2;
+let userId3;
 
 jest.setTimeout(10000);
 
@@ -15,31 +19,52 @@ Change the database in database.js before running tests
 
 describe('Testing GET on /api/clients', () => {
 
+    const fakeUser1 = {
+        username: 'group07@gmail.com',
+        password: 'abc123',
+        type: 'client'
+    };
 
-    const fakeClient1 = {
-        userid: 1,
-        name: 'John',
-        surname: 'Doe',
-        wallet: 50.30,
-        address: 'Corso Duca degli Abruzzi, 21, Torino'
-    }
-    const fakeClient2 = {
-        userid: 2,
-        name: 'Mario',
-        surname: 'Rossi',
-        wallet: 12.30,
-        address: 'Corso Mediterraneo, 70, Torino'
-    }
+    const fakeUser2 = {
+        username: 'group07-1@gmail.com',
+        password: 'abc123',
+        type: 'farmer'
+    };
 
-
+    const fakeUser3 = {
+        username: 'group07-2@gmail.com',
+        password: 'abc123',
+        type: 'client'
+    };
     beforeEach(async () => {
         //clear and fill (mock) client database with fakeClient1 and fakeClient2
+        await userDao.deleteAllUsers();
+        userId1 = await userDao.insertUser(fakeUser1);
+        userId2 =  await userDao.insertUser(fakeUser2);
+        userId3 =  await userDao.insertUser(fakeUser3);
+
+        const fakeClient1 = {
+            userid: userId1,
+            name: 'John',
+            surname: 'Doe',
+            wallet: 50.30,
+            address: 'Corso Duca degli Abruzzi, 21, Torino'
+        }
+        const fakeClient2 = {
+            userid: userId2,
+            name: 'Mario',
+            surname: 'Rossi',
+            wallet: 12.30,
+            address: 'Corso Mediterraneo, 70, Torino'
+        }
+
         await clientDao.deleteAllClients();
         await clientDao.insertClient(fakeClient1);
         await clientDao.insertClient(fakeClient2);
     });
 
     afterEach(async () => {
+        await userDao.deleteAllUsers();
         await clientDao.deleteAllClients();
     });
 
@@ -48,14 +73,14 @@ describe('Testing GET on /api/clients', () => {
     test("It should respond with an array of clients", async () => {
         const response = await request(app).get('/api/clients');
         const fakeClient1 = {
-            userid: 1,
+            userid: userId1,
             name: 'John',
             surname: 'Doe',
             wallet: 50.30,
             address: 'Corso Duca degli Abruzzi, 21, Torino'
         }
         const fakeClient2 = {
-            userid: 2,
+            userid: userId2,
             name: 'Mario',
             surname: 'Rossi',
             wallet: 12.30,
@@ -75,16 +100,18 @@ describe('Testing POST on /api/clients', () => {
     });
 
     test('It should respond with 200 status code', async () => {
-        const response = await request(app).post('/api/clients').send({
+        const response = await request(app).post('/api/client').send({
+            userid: userId1,
             name: 'Grrmafa',
             surname: 'Idcamcv',
+            email: 'Idcamcv@gmail.com',
             wallet: 50.30,
             address: 'Corso Duca degli Abruzzi, 21, Torino'
         })
         expect(response.statusCode).toBe(200);
     });
 
-    describe.skip('It should respond with 400 (Bad Request) status code', () => {
+    describe('It should respond with 400 (Bad Request) status code', () => {
 
         test('Case of one parameter missing', async () => {
             const obj = {
@@ -105,6 +132,7 @@ describe('Testing POST on /api/clients', () => {
 
         test("Case of wrong 'name' parameter type", async () => {
             const response = await request(app).post('/api/clients').send({
+                userid: userId1,
                 name: 1,
                 surname: 'Idcamcv',
                 wallet: 50.30,
@@ -115,6 +143,7 @@ describe('Testing POST on /api/clients', () => {
 
         test("Case of wrong 'surname' parameter type", async () => {
             const response = await request(app).post('/api/clients').send({
+                userid: userId1,
                 name: 'Grrmafa',
                 surname: 1,
                 wallet: 50.30,
@@ -125,6 +154,7 @@ describe('Testing POST on /api/clients', () => {
 
         test("Case of wrong 'wallet' parameter type", async () => {
             const response = await request(app).post('/api/clients').send({
+                userid: userId1,
                 name: 'Grrmafa',
                 surname: 'Idcamcv',
                 wallet: 'a',
@@ -135,6 +165,7 @@ describe('Testing POST on /api/clients', () => {
 
         test("Case of wrong 'address' parameter type", async () => {
             const response = await request(app).post('/api/clients').send({
+                userid: userId1,
                 name: 'Grrmafa',
                 surname: 'Idcamcv',
                 wallet: 'a',
