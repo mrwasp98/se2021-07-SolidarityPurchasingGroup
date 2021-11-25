39194676fe6ccc1468ec925473a5cc1b2db56e31
mosi14 <m.asadollahy@hotmail.com>
2021-11-25 09:54:04 +0100

    fix modal

--- client/src/Components/MyModal.js
+++ client/src/Components/MyModal.js
@@ -1,31 +1,25 @@
-//import { useEffect, useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button,Container } from "react-bootstrap";
 import { topUpWallet } from "./Icons";
 
 export default function MyModal(props) {
-    //const [show, setShow] = useState(true);
-console.log("Modal: "+props.show);
-   // const handleClose = () => setShow(false);
-   // const handleShow = () => setShow(true);
-  
+
     return (
       <>
-        {/* <Button variant="primary" onClick={handleShow}>
-          
-        </Button>
-   */}
         <Modal show={props.show} onHide={props.close}>
           <Modal.Header closeButton>
             <Modal.Title>Not suficient budget {topUpWallet}</Modal.Title>
           </Modal.Header>
           <Modal.Body>Do you want to increase your wallet!? </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={props.close}>
+          <Container className="d-flex justify-content-between my-4">
+            
+          <Button variant="secondary" className="float-left" onClick={props.close}>
               Close
             </Button>
             <Button variant="primary" onClick={props.close}>
               Top Up Your Wallet
             </Button>
+            </Container>
           </Modal.Footer>
         </Modal>
       </>
