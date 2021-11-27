da50812d426f03780a9c0382e17961ecb0a50b5d
kecco98 <kecco98@outlook.com>
2021-11-27 18:51:36 +0100

    GUI story 6

---
+++ client/src/Components/Type.js
@@ -0,0 +1,31 @@
+import { Card, Button, ButtonGroup, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Type() {
+    return (
+        <Container className="justify-content-center">
+            <Card className="mt-4">
+                <Card.Header as="h5">Choose the account type</Card.Header>
+                <Card.Body className="mb-2">
+                    <ButtonGroup vertical aria-label="Directions" className="d-flex" >
+                        <Button variant="yellow" className="mx-auto d-flex p-0 mb-4" size="lg" id="toprodreq">
+                            <Link style={{ minWidth: "100%", textDecoration: "none" }} to="/client" className="py-2 yellowLink">
+                                Client
+                            </Link>
+                        </Button>
+                        <Button variant="yellow" className="mx-auto d-flex p-0 mb-4" size="lg" id="toregcl">
+                            <Link style={{ minWidth: "100%", textDecoration: "none" }} to="/farmer"className="py-2 yellowLink">
+                                Farmer
+                            </Link>
+                        </Button>
+                        <Button variant="yellow" className="mx-auto d-flex p-0 mb-4" size="lg" id="topup">
+                            <Link style={{ minWidth: "100%", textDecoration: "none" }} to="/shopemployee" className="py-2 yellowLink">
+                                Shop Employee
+                            </Link>
+                        </Button>
+                    </ButtonGroup>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+}
