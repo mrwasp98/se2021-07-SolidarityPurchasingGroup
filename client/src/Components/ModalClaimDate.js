c45b4eceb424d55f97189ae7241d178e1a92a59e
mosi14 <m.asadollahy@hotmail.com>
2021-12-10 15:29:43 +0100

    add choose address for delivery

--- client/src/Components/ModalClaimDate.js
+++ client/src/Components/ModalClaimDate.js
@@ -1,76 +1,186 @@
 /*
 This component is used to let the client choose time and date for the pickup
 */
-import { Form, Button, Modal, Row, Col, Container, } from "react-bootstrap";
-import { useState, } from "react";
-import DatePicker from "react-datepicker"
+import {
+  Form,
+  ListGroup,
+  Button,
+  Modal,
+  Row,
+  Col,
+  Container,
+} from "react-bootstrap";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function ModalClaimDate(props) {
-    const handleClose = () => {
-        if(!(props.claimdate.getDay() ==0 ||props.claimdate.getDay() ==1 ||props.claimdate.getDay() ==2|| props.claimdate.getDay() ==6 )){
-            props.handleOrder()
-            props.setShow(false)
-        }
-        
-    };
+  const [orderMethod, setOrderMethod] = useState("store");
+  const [address, setAddress] = useState("");
+  const [showAddressForm, setShowAddressForm] = useState(false);
 
-    const notSelectableDates = (date) => {
-        const day = date.getDay();
-        return day !== 0 && day !== 1 && day !== 2 && day !== 6;
-    };
+  const handleClose = () => {
+    if (
+      !(
+        props.claimdate.getDay() == 0 ||
+        props.claimdate.getDay() == 1 ||
+        props.claimdate.getDay() == 2 ||
+        props.claimdate.getDay() == 6
+      )
+    ) {
+      props.handleOrder();
+      props.setShow(false);
+    }
+  };
 
-    const notSelectableTimes = (time) => {
-        const start = new Date(time);
-        const end = new Date(time);
-        const selectedDate = new Date(time);
+  const notSelectableDates = (date) => {
+    const day = date.getDay();
+    return day !== 0 && day !== 1 && day !== 2 && day !== 6;
+  };
 
-        start.setHours(9, 0, 0);
-        end.setHours(21, 0, 0)
+  const notSelectableTimes = (time) => {
+    const start = new Date(time);
+    const end = new Date(time);
+    const selectedDate = new Date(time);
 
-        return start.getTime() < selectedDate.getTime() && end.getTime() > selectedDate.getTime()
-    };
-
+    start.setHours(9, 0, 0);
+    end.setHours(21, 0, 0);
 
     return (
-        <>
-            <Modal show={props.show} onHide={handleClose} backdrop="static" keyboard={false} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Please, select date and time to pick up your order</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div>
-                        Remember, pickups take place from
-                        <strong> Wednesday morning (09:00) </strong>
-                        until
-                        <strong> Friday evening (21:00)</strong>
-                        .
-                    </div>
-                    <Row className="text-center mt-3 mb-2 ">
-                        <DatePicker
-                            selected={props.claimdate}
-                            onChange={(date) => props.setClaimdate(date)}
-                            filterDate={notSelectableDates}
-                            filterTime={notSelectableTimes}
-                            dateFormat="dd-mm-yyyy HH:mm"
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                        />
-                    </Row>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Col>
-                        {/* <Button variant="secondary" onClick={handleClose}>
+      start.getTime() < selectedDate.getTime() &&
+      end.getTime() > selectedDate.getTime()
+    );
+  };
+
+  return (
+    <>
+      <Modal
+        show={props.show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Please, select date and time to pick up your order
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            Remember, pickups take place from
+            <strong> Wednesday morning (09:00) </strong>
+            until
+            <strong> Friday evening (21:00)</strong>.
+          </div>
+          <ListGroup className="mt-2">
+            <ListGroup.Item>
+              <Form>
+                <Form.Check
+                  inline
+                  defaultChecked
+                  className="m-3 "
+                  type="radio"
+                  name="ordergroup"
+                  id="checkstore"
+                  label="Pickup in store"
+                  onClick={() => setOrderMethod(() => "store")}
+                />
+                <Form.Check
+                  inline
+                  type="radio"
+                  name="ordergroup"
+                  id="checkdelivery"
+                  label="Deliver to address"
+                  onClick={() => setOrderMethod(() => "address")}
+                />
+              </Form>
+            </ListGroup.Item>
+            <ListGroup.Item
+              variant="primary"
+              className="d-flex w-100 justify-content-center align-items-center"
+            >
+              <Form>
+                {orderMethod === "address" && (
+                  <>
+                   <Form.Check
+                      defaultChecked
+                      type="radio"
+                      name="addressgroup"
+                      id="checkdefadd"
+                      label="Default address"
+                      onClick={() => {
+                        setAddress(() => "");
+                        setShowAddressForm(() => false);
+                      }}
+                    />
+                    <Form.Check                     
+                      type="radio"
+                      name="addressgroup"
+                      id="checkspec"
+                      label=" Other address"
+                      onClick={() => {
+                        setShowAddressForm(() => true);
+                      }}
+                    />
+                   {" "}
+                  </>
+                )}
+                {orderMethod === "address" && showAddressForm &&(
+                  <>
+                    <Form.Label className="mt-3">
+                      address:
+                    </Form.Label>
+                    <Form.Control
+                    
+                      type="text"
+                      placeholder="Write your delivery address"
+                      onChange={(e) => setAddress(() => e.target.value)}
+                    />
+                  </>
+                )}
+
+                <Form.Label
+                  inline
+                  className={orderMethod === "store" ? "mt-0" : "mt-3"}
+                >
+                  Choose time for delivery:
+                </Form.Label>
+                <DatePicker
+                  selected={props.claimdate}
+                  onChange={(date) => props.setClaimdate(date)}
+                  filterDate={notSelectableDates}
+                  filterTime={notSelectableTimes}
+                  dateFormat="dd-mm-yyyy HH:mm"
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                />
+              </Form>
+            </ListGroup.Item>
+          </ListGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Col>
+            {/* <Button variant="secondary" onClick={handleClose}>
                             Close
                         </Button> */}
-                    </Col>
-                    <Col className="d-flex justify-content-end">
-                        <Button variant="primary" onClick={handleClose}
-                        disabled={props.claimdate.getDay() ==0 ||props.claimdate.getDay() ==1 ||props.claimdate.getDay() ==2|| props.claimdate.getDay() ==6}
-                        >Check and Order</Button>
-                    </Col>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+          </Col>
+          <Col className="d-flex justify-content-end">
+            <Button
+              variant="primary"
+              onClick={handleClose}
+              disabled={
+                props.claimdate.getDay() == 0 ||
+                props.claimdate.getDay() == 1 ||
+                props.claimdate.getDay() == 2 ||
+                props.claimdate.getDay() == 6
+              }
+            >
+              Check and Order
+            </Button>
+          </Col>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
