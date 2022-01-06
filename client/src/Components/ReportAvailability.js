990f173d7ecc221ca3b6ed83ecd07aca8e68f98a
ANTONIO VESPA s284101 <44071720+mrwasp98@users.noreply.github.com>
2022-01-06 19:44:52 +0100

    update

--- client/src/Components/ReportAvailability.js
+++ client/src/Components/ReportAvailability.js
@@ -105,12 +105,12 @@ function ProductAvailableRow(props) {
     }
 
     return (<tr className="align-middle">
-        <td style={{ fontSize: "18pt" }}>{product.name}</td>
+        <td style={{ fontSize: "17px" }}>{product.name}</td>
         <td>
             <Form.Group controlId="formBasicPrice" >
                 {"€ "}
                 <input
-                    className="align-baseline"
+                    className="align-baseline inputFarmerTable"
                     type="number"
                     min={1}
                     max={1000}
@@ -120,13 +120,15 @@ function ProductAvailableRow(props) {
                 />
             </Form.Group>
         </td>
-        <td><Image style={{ width: "100px" }} src={product.picture} fluid /></td>
-        <td>{(quantity > -1) ?
-            <span style={{ cursor: 'pointer' }} className={"add-btn-" + props.index} onClick={add}>{iconAdd}</span>
-            : <span style={{ cursor: 'pointer' }}>{iconAddDisabled}</span>}&nbsp;
-            {quantity + " " + product.measure + " "}
-            {quantity > 0 ? <span style={{ cursor: 'pointer' }} className={"sub-btn-" + props.index} onClick={sub}>{iconSub}</span>
-                : <span style={{ cursor: 'pointer' }}>{iconSubDisabled}</span>}
+        <td><Image style={{ width: "130px" }} src={product.picture} fluid /></td>
+        <td>
+            <Row>{(quantity > -1) ?
+                <Col xs={3} style={{ cursor: 'pointer' }} className={"add-btn-" + props.index} onClick={add}>{iconAdd}</Col>
+                : <Col xs={3} style={{ cursor: 'pointer' }}>{iconAddDisabled}</Col>}&nbsp;
+                <Col xs={3}>{quantity + " " + product.measure + " "}</Col>
+                {quantity > 0 ? <Col xs={3} style={{ cursor: 'pointer' }} className={"sub-btn-" + props.index} onClick={sub}>{iconSub}</Col>
+                    : <Col xs={3} style={{ cursor: 'pointer' }}>{iconSubDisabled}</Col>}
+            </Row>
         </td>
     </tr>
     )
@@ -153,6 +155,7 @@ export default function ReportAvailability(props) {
     const [dateavailability, setDateavailability] = useState(dayjs(props.date).add(1, 'day').format('YYYY-MM-DD'))
     const [productsAvailable, setProductsAvailable] = useState([]);
     const [dirty, setDirty] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
     const [dirtyO, setDirtyO] = useState(false);
 
     /** This function sets the date of availability 
@@ -181,6 +184,14 @@ export default function ReportAvailability(props) {
                 break;
         }
     }
+
+    useEffect(() => {
+        if (showAlert === true) {
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 1500);
+        }
+    }, [showAlert]);
 
     // this useEffect gets all the product of a particular farmer
     useEffect(() => {
@@ -234,13 +245,20 @@ export default function ReportAvailability(props) {
                 props.bot.sendMessage(chatId, "Il farmer " + props.username + " ha pubblicato nuovi prodotti!")
             )
         }
+        setShowAlert(true);
         setDirty(true)
     }
 
-    return (<Container className="justify-content-center">
+    return (<Container className="justify-content-center pt-4">
         <h1>Hello <i>{props.username}</i>!</h1>
         <hr></hr>
         <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+            {showAlert ?
+                <Alert variant="success" className="m-0"
+                    style={{position:"fixed", width:"90%", top: '3rem', zIndex: '200', background: '#14B8B8', color: "white" }}>Report sent successfully!</Alert>
+                :
+                <></>
+            }
             <Row>
                 <Col sm={3} style={{ backgroundColor: '#f2f2f2' }}>
                     <ListGroup variant="flush" className="mt-3">
@@ -263,13 +281,13 @@ export default function ReportAvailability(props) {
                         }
                     </ListGroup>
                 </Col>
-                <Col sm={9}>
+                <Col sm={9} className="p-0 m-0">
                     <Tab.Content>
                         <Tab.Pane eventKey="#link1">
                             <Col>
                                 <h3>These are all your products</h3>
                             </Col>
-                            <Row >
+                            <Row className="p-1">
                                 <Col className="d-flex justify-content-start align-items-center">
                                     <Form>
                                         <Form.Group controlId="formProduct">
@@ -282,25 +300,27 @@ export default function ReportAvailability(props) {
                                     <Link to={{ pathname: "/addProduct" }}><Button id="farmer_add_product">Add product</Button></Link>
                                 </Col>
                             </Row>
-                            <Table className="mt-4" striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Description</th>
-                                        <th>Image</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {products.filter(p => p.name.toLowerCase().includes(product) || p.name.toUpperCase().includes(product)).map((product, index) => <ProductRow key={product.id} index={index} farmerid={props.userId} product={product} deleteProd={deleteProd}></ProductRow>)}
-                                </tbody>
-                            </Table>
+                            <Row>
+                                <Table className="mt-4 tabFarmer" striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Description</th>
+                                            <th>Image</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {products.filter(p => p.name.toLowerCase().includes(product) || p.name.toUpperCase().includes(product)).map((product, index) => <ProductRow key={product.id} index={index} farmerid={props.userId} product={product} deleteProd={deleteProd}></ProductRow>)}
+                                    </tbody>
+                                </Table>
+                            </Row>
                         </Tab.Pane>
                         {!((props.date.getDay() === 6 && dayjs(props.date).hour() >= 9) || props.date.getDay() === 0 || (props.date.getDay() === 1 && dayjs(props.date).hour() < 9)) ? <>
-                            <Tab.Pane eventKey="#link2">
+                            <Tab.Pane eventKey="#link2" className="p-4">
                                 <h3>Report the availability for the next week</h3>
                                 <span className='text-muted'>Select the quantities you expect to deliver for next week. When you're done, click on the light blue button.</span>
-                                <Table className="mt-3" striped bordered hover responsive>
+                                <Table className="mt-3 " striped bordered hover responsive>
                                     <thead>
                                         <tr>
                                             <th>Name</th>
