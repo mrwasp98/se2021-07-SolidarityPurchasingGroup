0da405e23657d3b461f3b81f690db12878f6deb5
giacomino100 <giacomo.inghilleri@gmail.com>
2022-01-11 10:48:23 +0100

    fix notifications

--- client/src/Components/Homepages/WarehouseHome.js
+++ client/src/Components/Homepages/WarehouseHome.js
@@ -1,13 +1,11 @@
-import dayjs from 'dayjs';
 import React from 'react';
-import { Container, Alert } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export default function WarehouseHome(props) {
 
     return (
         <Container className="p-4">
             <h1>Hi warehouse manager</h1>
-
         </Container>
     );
 }
